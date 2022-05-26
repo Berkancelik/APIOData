dbb2228dbc97828f891f259eda63621dcf00ac19
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-26 03:44:06 +0300

    Code First and some fixed

---
+++ APIOData.API/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace APIOData.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set;}
+    }
+}
---
+++ APIOData.API/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace APIOData.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime? Created { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
