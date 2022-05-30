077677093275d9caf651b0adbd98dc29a54e6e71
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-30 07:36:54 +0300

    ODta Connected Service Extension

---
+++ ODataConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ODataConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
